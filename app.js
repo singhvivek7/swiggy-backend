979fa6fab8b0eee7e51283d33e60a4d055a7ca0f
Vivek Kumar <mail.help.vivek@gmail.com>
2023-12-17 15:26:54 +0530

    change

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const env = require('dotenv');
-const corsOptions = require('./config/corsOptions');
-const userRoutes = require('./routes/userRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const restaurantRoutes = require('./routes/restaurantRoutes');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const env = require("dotenv");
+const corsOptions = require("./config/corsOptions");
+const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const restaurantRoutes = require("./routes/restaurantRoutes");
 
 const app = express();
 
@@ -32,17 +32,24 @@ const port = process.env.PORT || 2727;
 
 connectDB().then(() => {
   app.listen(port, () => {
-    console.log('listening for requests at 2727');
+    console.log("listening for requests at 2727");
   });
 });
 
-app.use('/api/v1/user', userRoutes);
-app.use('/api/v1/order', orderRoutes);
-app.use('/api/v1/restaurant', restaurantRoutes);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/order", orderRoutes);
+app.use("/api/v1/restaurant", restaurantRoutes);
 
-app.all('*', (req, res) => {
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: `Server is up`,
+  });
+});
+
+app.all("*", (req, res) => {
   res.status(400).json({
-    status: 'error',
+    status: "error",
     message: `Can't find ${req.originalUrl} on this server!`,
   });
 });
